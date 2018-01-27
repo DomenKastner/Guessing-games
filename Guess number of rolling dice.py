9acf4dcada3f7fd23cb1dd500b1085d10c9d3a16
DomenKastner <domen.kastner@gmail.com>
2018-01-27 19:22:05 +0100

    Rolling dice game

---
+++ Guess number of rolling dice.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import random
+# Program simuliranja kocke do številke 6
+print("Pozdravljeni v simulatorju metanja igralne kocke")
+
+print("I---------I")
+print("I  0      I")
+print("I    0    I")
+print("I      0  I")
+print("I---------I")
+
+while True:
+
+    value = random.randint(1, 6)
+    guesses = 0
+    guess = int(raw_input("Ugani na katero številko bo padla kocka? : "))
+
+    if guess != value:
+        print ("Škoda, narobe ste uganili! Poskusite še enkrat. ")
+
+    else:
+        print (" Bravo")
+
+        break
