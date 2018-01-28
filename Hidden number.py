2ae6fc181cc7fd1e0c11575e6c4124b467fc79d7
DomenKastner <domen.kastner@gmail.com>
2018-01-28 21:02:45 +0100

    ...

--- Hidden number.py
+++ Hidden number.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+
+import random
+
+
+def main():
+
+    x = random.randint(1, 10)
+    # print x
+
+    while True:
+
+        guess = int(raw_input("Ugani skrito število med 1 in 10 :"))
+
+        if guess == x:
+            print "Bravo"
+            break
+        elif guess == 0:
+            print "Nč v tej igri ne velja. Ugibaj ponovno"
+        elif guess >= 11:
+            print "Vpisal si previsoko številko. Ugibaš samo od 1 do 10!"
+
+
+main()
--- Loto Generator.py
+++ Loto Generator.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+
+#  Loto Genereator Številk
+
+import random
+
+
+def stevilke():
+
+    list_st = []
+
+  # for number in range(0, 7):
+    for number in range(1):
+      # list_st.append(random.sample(0, 40))
+        list_st.append(random.sample(xrange(1, 40), 7))  # Če prav zastopim xrange ne ponavlja sekvence x števil?
+    return list_st
+
+
+print "Generirane številke so naslednje: {} ". format(stevilke())
+
+if __name__ == '__stevilke__':
+  stevilke()
